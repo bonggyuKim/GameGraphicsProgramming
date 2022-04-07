39c8cef9aa294d00cf0720dfa95df1dffed3911f
bonggyuKim <50454395+bonggyuKim@users.noreply.github.com>
2022-04-08 02:29:54 +0900

    Add files via upload

---
+++ Source/Library/Window/BaseWindow.h
@@ -0,0 +1,233 @@
+#pragma once
+
+#include "Common.h"
+
+namespace library
+{
+
+    template <class DerivedType>
+    class BaseWindow
+    {
+    public:
+        static LRESULT CALLBACK WindowProc(_In_ HWND hWnd, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam);
+
+        BaseWindow();
+        BaseWindow(const BaseWindow& rhs) = delete;
+        BaseWindow(BaseWindow&& rhs) = delete;
+        BaseWindow& operator=(const BaseWindow& rhs) = delete;
+        BaseWindow& operator=(BaseWindow&& rhs) = delete;
+        virtual ~BaseWindow() = default;
+
+        virtual HRESULT Initialize(_In_ HINSTANCE hInstance, _In_ INT nCmdShow, _In_ PCWSTR pszWindowName) = 0;
+        virtual PCWSTR GetWindowClassName() const = 0;
+        virtual LRESULT HandleMessage(_In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam) = 0;
+
+        HWND GetWindow() const;
+
+    protected:
+        HRESULT initialize(
+            _In_ HINSTANCE hInstance,
+            _In_ INT nCmdShow,
+            _In_ PCWSTR pszWindowName,
+            _In_ DWORD dwStyle,
+            _In_opt_ INT x = CW_USEDEFAULT,
+            _In_opt_ INT y = CW_USEDEFAULT,
+            _In_opt_ INT nWidth = CW_USEDEFAULT,
+            _In_opt_ INT nHeight = CW_USEDEFAULT,
+            _In_opt_ HWND hWndParent = nullptr,
+            _In_opt_ HMENU hMenu = nullptr
+        );
+
+        HINSTANCE m_hInstance;
+        HWND m_hWnd;
+        LPCWSTR m_pszWindowName;
+    };
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   BaseWindow<DerivedType>::WindowProc
+      Summary:  Defines the behavior of the window—its appearance, how
+                it interacts with the user, and so forth
+      Args:     HWND hWnd
+                  Handle to the window
+                UINT uMessage
+                  Message code
+                WPARAM wParam
+                  Additional data the pertains to the message
+                LPARAM lParam
+                  Additional data the pertains to the message
+      Modifies: [m_hWnd].
+      Returns:  LRESULT
+                  Integer value that your program returns to Windows
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+    template<class DerivedType>
+    inline LRESULT BaseWindow<DerivedType>::WindowProc(_In_ HWND hWnd, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam)
+    {
+        DerivedType* pThis = nullptr;
+
+        if (uMsg == WM_NCCREATE)
+        {
+            CREATESTRUCT* pCreate = reinterpret_cast<CREATESTRUCT*>(lParam);
+            pThis = reinterpret_cast<DerivedType*>(pCreate->lpCreateParams);
+            SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pThis));
+
+            pThis->m_hWnd = hWnd;
+        }
+        else
+        {
+            pThis = reinterpret_cast<DerivedType*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+        }
+
+        if (pThis)
+        {
+            return pThis->HandleMessage(uMsg, wParam, lParam);
+        }
+
+        return DefWindowProc(hWnd, uMsg, wParam, lParam);
+    }
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   BaseWindow<DerivedType>::BaseWindow
+      Summary:  Constructor
+      Modifies: [m_hInstance, m_hWnd, m_pszWindowName].
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+    template<class DerivedType>
+    inline BaseWindow<DerivedType>::BaseWindow()
+        : m_hInstance(nullptr)
+        , m_hWnd(nullptr)
+        , m_pszWindowName(nullptr)
+    {
+    }
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   BaseWindow<DerivedType>::GetWindow()
+      Summary:  Returns the handle to the window
+      Returns:  HWND
+                  The handle to the window
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+    template<class DerivedType>
+    inline HWND BaseWindow<DerivedType>::GetWindow() const
+    {
+        return m_hWnd;
+    }
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   BaseWindow<DerivedType>::initialize
+      Summary:  Registers the window class and creates a window
+      Args:     HINSTANCE hInstance
+                  Handle to the instance
+                INT nCmdShow
+                  Is a flag that says whether the main application window
+                  will be minimized, maximized, or shown normally
+                PCWSTR pszWindowName
+                  The window name
+                DWORD dwStyle
+                  The style of the window being created
+                INT x
+                  The initial horizontal position of the window
+                INT y
+                  The initial vertical position of the window
+                INT nWidth
+                  The width, in device units, of the window
+                INT nHeight
+                  The height, in device units, of the window
+                HWND hWndParent
+                  A handle to the parent or owner window of the window
+                  being created
+                HMENU hMenu
+                  A handle to a menu, or specifies a child-window
+                  identifier depending on the window style
+      Modifies: [m_hInstance, m_pszWindowName, m_hWnd].
+      Returns:  HRESULT
+                  Status code
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+    template<class DerivedType>
+    inline HRESULT BaseWindow<DerivedType>::initialize(
+        _In_ HINSTANCE hInstance,
+        _In_ INT nCmdShow,
+        _In_ PCWSTR pszWindowName,
+        _In_ DWORD dwStyle,
+        _In_opt_ INT x,
+        _In_opt_ INT y,
+        _In_opt_ INT nWidth,
+        _In_opt_ INT nHeight,
+        _In_opt_ HWND hWndParent,
+        _In_opt_ HMENU hMenu)
+    {
+        // Register the window class
+        WNDCLASSEX wcex =
+        {
+            .cbSize = sizeof(WNDCLASSEX),
+            .style = CS_HREDRAW | CS_VREDRAW,
+            .lpfnWndProc = WindowProc,
+            .cbClsExtra = 0,
+            .cbWndExtra = 0,
+            .hInstance = hInstance,
+            .hIcon = LoadIcon(hInstance, reinterpret_cast<LPCWSTR>(IDI_TUTORIAL)),
+            .hCursor = LoadCursor(nullptr, IDC_ARROW),
+            .hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1),
+            .lpszMenuName = nullptr,
+            .lpszClassName = GetWindowClassName(),
+            .hIconSm = LoadIcon(wcex.hInstance, reinterpret_cast<LPCWSTR>(IDI_TUTORIAL))
+        };
+
+        if (!RegisterClassEx(&wcex))
+        {
+            DWORD dwError = GetLastError();
+
+            MessageBox(
+                nullptr,
+                L"Call to RegisterEx failed!",
+                PSZ_COURSE_TITLE,
+                NULL
+            );
+
+            if (dwError != ERROR_CLASS_ALREADY_EXISTS)
+            {
+                return HRESULT_FROM_WIN32(dwError);
+            }
+
+            return E_FAIL;
+        }
+
+        // Create window
+        m_hInstance = hInstance;
+        m_pszWindowName = pszWindowName;
+        RECT rc = { 0, 0, nWidth, nHeight };
+        AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+        m_hWnd = CreateWindow(
+            GetWindowClassName(),                                       // Window class
+            m_pszWindowName,                                            // Window text
+            dwStyle,   // Window style
+
+            // Size and Position
+            x, y, rc.right - rc.left, rc.bottom - rc.top,
+            hWndParent,    // Parent window
+            hMenu,    // Menu
+            m_hInstance,  // Instance handle
+            this     // Additiona;l application data
+        );
+
+        if (!m_hWnd)
+        {
+            DWORD dwError = GetLastError();
+
+            MessageBox(
+                nullptr,
+                L"Call to CreateWindowEx failed!",
+                PSZ_COURSE_TITLE,
+                NULL
+            );
+
+            if (dwError != ERROR_CLASS_ALREADY_EXISTS)
+            {
+                return HRESULT_FROM_WIN32(dwError);
+            }
+
+            return E_FAIL;
+        }
+
+        ShowWindow(m_hWnd, nCmdShow);
+
+        return S_OK;
+    }
+}
